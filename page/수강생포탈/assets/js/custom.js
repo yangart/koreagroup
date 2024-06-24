11ebd8524a6547e6cb9470309a4894655a375e00
somi <some606@gmail.com>
2024-06-24 22:08:24 +0900

    - 수강생포털 메인

--- page/수강생포탈/assets/js/custom.js
+++ page/수강생포탈/assets/js/custom.js
@@ -48,3 +48,26 @@ function openMoAside(event){
         menuAside.classList.toggle('open');
     }
 }
+
+var eventCarousel = document.querySelector('#eventCarousel')
+var carousel = new bootstrap.Carousel(eventCarousel, {
+   interval: 0
+});
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 2;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
